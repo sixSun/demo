f1cf9d70ca5f22f5c3a51ec5aeaabef9ee6cfba4
sixSun <m15600908932@163.com>
2016-06-29 20:40:56 +0800

    小测验
    localStorage运用

---
+++ 小测验/demo.js
@@ -0,0 +1,31 @@
+﻿
+$(function() {	
+
+var count = 0;
+$(".temp_inner").spectrum({
+    color: "#f60",
+	change : function (color) {		
+            $('li.myLi:last').before('<div  class ="nc"> <li class="myLi new-color"  style =" background-color :  '+$(".temp_inner").spectrum("get").toHexString()+' ; "><span class="delColor">x</span></li> </div>');            			
+			
+			var str = $('.nc').get(count).innerHTML;
+			count +=1;
+			var key = "nli_"+localStorage.length; 
+			localStorage.setItem(key,str);
+
+			$('.delColor').click(function(){
+                $(this).parent('.new-color').remove();
+            });
+		}
+});
+})
+window.onload = function () {
+	var myColor = document.getElementsByTagName("ul")[0];
+	var addcolor = document.getElementById("addcolor");
+	var nc = document.createElement("div");
+	for (var i=0;i <localStorage.length;i++){
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		nc.innerHTML += value;		
+	}	
+	addcolor.parentNode.insertBefore(nc,addcolor);		
+	}
